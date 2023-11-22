70e7e2ebf550a3bb806f556de175fa107fd1d026
fabian <fabianalmendro456@gmail.com>
2023-11-22 16:12:22 -0400

    type llenado

---
+++ src/pages/types.ts
@@ -0,0 +1,12 @@
+export interface DatosCorridasType {
+    trema: number;
+    porcentajeAceptacion: number;
+    numeroCorridas: number;
+    numeroAnios: number;
+    valorMinimoInversion: number;
+    valorMaximoInversion: number;
+    valorProbableInversion: number;
+    valorMinimoFlujoNeto: number;
+    valorMaximoFlujoNeto: number;
+    valorProbableFlujoNeto: number;
+  }
