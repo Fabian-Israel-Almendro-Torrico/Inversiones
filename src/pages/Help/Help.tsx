7b21728d85abaa40a497489186b92f79d5153b91
RodriBau <rodri.bp.gg@gmail.com>
2023-12-03 20:27:30 -0400

    COMIENZO AYUDA 1.4

--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -43,26 +43,26 @@ const Help: React.FC = () => {
       </div>
     </IonHeader>
       <IonContent id='conte'>
-      <h1 id="info-title">¿Problemas?</h1>
-      <p id="inicio-text">Acá te enseñamos a usar un poco la aplicación</p>
+      <h1 id="titulo-problema">¿Problemas?</h1>
+      <p id="inicio-text">Acá te enseñamos a llenar esta parte 😁</p>
 
       <h2 id="titulo-ejemplo">TREMA - PROYECTO ACEPTADO</h2>
       <img id="help-1" src="https://cdn.discordapp.com/attachments/837905669138677770/1181026064668041266/image.png?ex=657f8f3c&is=656d1a3c&hm=d2b375ba7c85c45eca7c42f4ce499b94017a7d6f4cb4ac1b7a8dce2959f34576&" alt="HombreRosa" />
       <p id="texto-help"><strong>Trema: </strong>El trema es  <br /><strong>% Proyecto Aceptado: </strong>Se refiere al </p>
 
-      <h2 id="titulo-ejemplo">NÚMERO DE CORRIDAS - NÚMERO DE AÑOS</h2>
-      <img id="help-1" src="" alt="HombreRosa" />
+      <h2 id="titulo-ejemplo">CORRIDAS - AÑOS</h2>
+      <img id="help-1" src="https://cdn.discordapp.com/attachments/837905669138677770/1181026970516062218/image.png?ex=657f9014&is=656d1b14&hm=51fef8b435824301e669b5f534a4060ee2e2d55e836dced2dc24642325bf204d&" alt="HombreRosa" />
       <p id="texto-help"><strong>Corridas: </strong>Se refiere a  <br /><strong>Años: </strong>Se refiere al </p>
 
       <h2 id="titulo-ejemplo">INVERSIÓN INICIAL</h2>
-      <img id="help-1" src="" alt="HombreRosa" />
+      <img id="help-1" src="https://cdn.discordapp.com/attachments/837905669138677770/1181027628791111801/image.png?ex=657f90b1&is=656d1bb1&hm=04a20a9152aa4f7c63865553ad1e69d8d972fcd7407c2e5cd3bd7e24d91c41ae&" alt="HombreRosa" />
       <p id="texto-help"><strong>Inversión Inicial: </strong>Se refiere a... y cuenta con 3 campos <br />
       <strong>Valor Minimo: </strong>Se refiere al <br />
       <strong>Valor Maximo: </strong>Se refiere al <br />
       <strong>Valor Probable: </strong>Se refiere al </p>
 
       <h2 id="titulo-ejemplo">FLUJO NETO</h2>
-      <img id="help-1" src="" alt="HombreRosa" />
+      <img id="help-1" src="https://cdn.discordapp.com/attachments/837905669138677770/1181028036032876554/image.png?ex=657f9112&is=656d1c12&hm=cf4780adf1a0c712d60a017fe26e73920c9023f1903182afdf161678afbed57c&" alt="HombreRosa" />
       <p id="texto-help"><strong>Flujo Neto: </strong>Se refiere a... y cuenta con 3 campos <br />
       <strong>Valor Minimo: </strong>Se refiere al <br />
       <strong>Valor Maximo: </strong>Se refiere al <br />
